daa6524e1bb9266fc144aa37377acb7106ee989e
Lauren Reimondo <lnr3604@rit.edu>
2014-11-11 17:39:56 -0500

    Added Title Screen and Images folder with the tower and lightning bolt images

---
+++ Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleScreen : MonoBehaviour {
+
+	private GUISkin skin;
+
+	// Use this for initialization
+	void Start () 
+	{
+		skin = Resources.Load("StartScreen") as GUISkin;
+	}
+	
+	// Update is called once per frame
+	void OnGUI() 
+	{
+		const int buttonWidth = 200;
+		const int instrButtonWidth = 200;
+		const int buttonHeight = 65;
+
+		GUI.skin = skin;
+
+		if(GUI.Button(new Rect(Screen.width/2 - (buttonWidth / 2), (2 * Screen.height / 3) - (buttonHeight / 2), buttonWidth, buttonHeight), "Start"))
+		{
+			Application.LoadLevel("GameScene");	
+		}
+
+		if(GUI.Button(new Rect(Screen.width/2 - (instrButtonWidth / 2), (2 * Screen.height / 2.5f) - (buttonHeight / 2), instrButtonWidth, buttonHeight), "How to Play"))
+		{
+			Application.LoadLevel("Instructions");
+		}
+	}
+}
