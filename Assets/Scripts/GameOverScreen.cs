dc733c549f6dbcffb1db317ac1a310998a3aa6ea
Eric Kipnis <erickipnis@yahoo.com>
2014-11-12 09:50:18 -0500

    Hooked up Game over screen.

---
+++ Assets/Scripts/GameOverScreen.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOverScreen : MonoBehaviour {
+
+	private GUISkin skin;
+
+	// Use this for initialization
+	void Start () {
+
+		skin = Resources.Load("GameOver") as GUISkin;
+	}
+
+	void OnGUI() 
+	{
+		const int buttonWidth = 200;
+		const int instrButtonWidth = 200;
+		const int buttonHeight = 65;
+		
+		GUI.skin = skin;
+		
+		if(GUI.Button(new Rect(Screen.width/2 - (buttonWidth / 2), (2 * Screen.height / 3) - (buttonHeight / 2), buttonWidth, buttonHeight), "Play Again?"))
+		{
+			Application.LoadLevel("GameScene");	
+		}
+		
+		if(GUI.Button(new Rect(Screen.width/2 - (instrButtonWidth / 2), (2 * Screen.height / 2.5f) - (buttonHeight / 2), instrButtonWidth, buttonHeight), "Return to Title"))
+		{
+			Application.LoadLevel("StartScreen");
+		}
+	}
+}
