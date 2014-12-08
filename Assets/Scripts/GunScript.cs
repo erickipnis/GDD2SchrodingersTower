6e043d4e4cc51dbaf41123c34fd2e6a88aa32335
chad c <chc8020@rit.edu>
2014-12-08 16:12:48 -0500

    Guns now kill.  No one can hide.  No one is safe

--- Assets/Scripts/GunScript.cs
+++ Assets/Scripts/GunScript.cs
@@ -41,20 +41,36 @@ public class GunScript : MonoBehaviour
 		// Movement
 		if (type == "side")
 		{
-			if (transform.position.y > 2.5)
+			if (transform.position.y > 2.9)
 				direction.y = -1;
-			else if (transform.position.y < -4.15)
+			else if (transform.position.y < -5.4)
 				direction.y = 1;
 		}
 		else if (type == "top")
 		{
-			if (transform.position.x > 6.5)
+			if (transform.position.x > 7.2)
 				direction.x = -1;
-			else if (transform.position.x < -6.5)
+			else if (transform.position.x < -7.2)
 				direction.x = 1;
 		}
 
 		movement = new Vector2(speed.x * direction.x, speed.y * direction.y);
+	}
+
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		if (coll.gameObject.tag == "Player")
+		{
+			GameObject[] guns = GameObject.FindGameObjectsWithTag("Gun");
+			for(int i = 0; i < guns.Length; i++)
+			{
+				DestroyImmediate(guns[i]);
+				StageScrolling.levelSurvived = 0;
+			}
+			
+			Destroy(coll.gameObject);
+			Application.LoadLevel("GameOver");
+		}
 	}
 
 	// Update Movement with RigidBody
@@ -100,4 +116,6 @@ public class GunScript : MonoBehaviour
 			return shootCooldown <= 0f;
 		}
 	}
+
+
 }
---
+++ Assets/Scripts/PlayerColl.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerColl : MonoBehaviour {
+
+	GameObject cat;
+	GameObject[] guns;
+
+
+	// Use this for initialization
+	void Start () {
+	
+		cat = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		guns = GameObject.FindGameObjectsWithTag("Gun");
+
+		if(cat.transform.position.x < -5.8f && cat.transform.position.y < -4f)
+		{
+			foreach(GameObject gun in guns)
+			{
+				if(gun.transform.position.y < -4.5f && gun.transform.position.x <= 0.1264596f)
+				{
+					Destroy(this.gameObject);
+					Application.LoadLevel("GameOver");
+				}
+			}
+		}
+		if( cat.transform.position.x > 6.3f && cat.transform.position.y < -4f)
+		{
+			foreach(GameObject gun in guns)
+			{
+				if(gun.transform.position.y < -4.5f && gun.transform.position.x >= 6.434641f)
+				{
+					Destroy(this.gameObject);
+					Application.LoadLevel("GameOver");
+				}
+			}
+		}
+	}
+
+
+}
